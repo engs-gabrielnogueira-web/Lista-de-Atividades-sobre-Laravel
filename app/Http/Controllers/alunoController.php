54b4528beaa74f7403174921e2c2c06b9f8d7466
Seu Nome Completo <engs-gabrielnogueira@camporeal.edu.br>
2026-09-09 20:26:20 -0300

    atv11: criacao de consultas eloquent por curso, nome, recentes e contagem total

--- app/Http/Controllers/alunoController.php
+++ app/Http/Controllers/alunoController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Aluno;
 use Illuminate\Http\Request;
 
-class alunoController extends Controller
+class AlunoController extends Controller
 {
-    //
+
+    public function porCurso($curso)
+    {
+        $alunos = Aluno::where('curso', $curso)->get();
+        return response()->json($alunos);
+    }
+    public function porNome($palavra)
+    {
+        $alunos = Aluno::where('nome', 'LIKE', "%{$palavra}%")->get();
+        return response()->json($alunos);
+    }
+
+    public function recentes()
+    {
+        $alunos = Aluno::where('created_at', '>=', now()->subDays(7))->get();
+        return response()->json($alunos);
+    }
+    public function quantidade()
+    {
+        $total = Aluno::count();
+        return response()->json(['total_alunos' => $total]);
+    }
 }
--- resources/views/alunos/index.blade.php
+++ resources/views/alunos/index.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
 
-@section('title', 'Listagem de Alunos')
+@section('title', 'Lista de Alunos')
 
 @section('content')
-    <h2>Lista de Alunos</h2>
+    <h2>Alunos Cadastrados</h2>
+
+    @php
+        $alunos = [
+            ['id' => 1, 'nome' => 'Gabriel'],
+            ['id' => 2, 'nome' => 'Ana']
+        ];
+    @endphp
+
+    @if(count($alunos) > 0)
+        <ul>
+            @foreach($alunos as $aluno)
+                <li>{{ $aluno['id'] }} - {{ $aluno['nome'] }}</li>
+            @endforeach
+        </ul>
+    @else
+        <p>Nenhum aluno encontrado.</p>
+    @endif
 @endsection
