5a398089225773a4937adc4c979f7be747c35bbc
Seu Nome Completo <engs-gabrielnogueira@camporeal.edu.br>
2026-09-09 20:06:25 -0300

    desafio: criacao do menu de navegacao compartilhado para todas as views

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/home">Home</a> | 
+    <a href="/alunos">Listar Alunos</a> | 
+    <a href="/alunos/create">Cadastrar Aluno</a>
+</nav>
+<hr>git add resources/views/partials/header.blade.php
