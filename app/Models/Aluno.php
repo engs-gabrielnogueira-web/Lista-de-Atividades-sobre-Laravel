50bec28c43457e7788f2e56652998a3db7ed0f82
Seu Nome Completo <engs-gabrielnogueira@camporeal.edu.br>
2026-09-09 21:56:17 -0300

    atv16: criacao do model Curso, migration e relacionamento belongsTo em Aluno

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -9,9 +9,10 @@ class Aluno extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'nome',
-        'email',
-        'cpf',
-    ];
+    protected $fillable = ['nome', 'email', 'cpf', 'curso_id'];
+
+    public function curso()
+    {
+        return $this->belongsTo(Curso::class);
+    }
 }
