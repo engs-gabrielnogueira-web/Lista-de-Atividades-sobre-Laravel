bec694bb234ebf3b4365f4065707132890382c6a
Seu Nome Completo <engs-gabrielnogueira@camporeal.edu.br>
2026-09-09 21:17:09 -0300

    atv14: criacao do formulario de cadastro de aluno com blade e csrf

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.app')
 
-@section('title', 'Novo Aluno')
-
 @section('content')
-    <h2>Cadastrar Novo Aluno</h2>
+    <h2>Cadastrar Aluno</h2>
+
+    <form action="{{ route('alunos.store') }}" method="POST">
+        @csrf
+
+        <div>
+            <label for="nome">Nome:</label><br>
+            <input type="text" id="nome" name="nome" value="{{ old('nome') }}" required>
+        </div>
+        <br>
+
+        <div>
+            <label for="email">E-mail:</label><br>
+            <input type="email" id="email" name="email" value="{{ old('email') }}" required>
+        </div>
+        <br>
+
+        <div>
+            <label for="cpf">CPF:</label><br>
+            <input type="text" id="cpf" name="cpf" value="{{ old('cpf') }}" required>
+        </div>
+        <br>
+
+        <button type="submit">Salvar Aluno</button>
+    </form>
 @endsection
